5fadf8f93513c5839c3aef93549d45a6585f4abc
rcalibrary <library@rca.ac.uk>
2024-07-11 12:11:03 +0100

    Update RCA_custom_shelfmap_button_test.js

--- RCA_custom_shelfmap_button_test.js
+++ RCA_custom_shelfmap_button_test.js
@@ -11,28 +11,3 @@ const summonlink = "<a target='_self' href='https://rca.summon.serialssolutions.
 document.getElementById('Quicksearch_NEWSEARCH').innerHTML = summonlink;
 }
 window.onload=summonback;
-
-
-// ShelfMap
-jQuery(document).ready(function() {
-  
-  var validSMLocs=["1:ONSHELF","1:REFERENCE","1:QR","1:PAMPHLET","1:PER_CURR","1:OVERSIZE","1:EAP","1:DYSLEXIA","1:FUELRCA","1:CRLCC","CRL Core Collection - Library Study Room","zReference Material"];
-  var pathname = window.location.pathname;
-  var bibid = pathname.match(/SD_ILS:.*[0-9]/).toString().replace('SD_ILS:','');
-
-  jQuery('tr.detailItemsTableRow td.detailItemsTable_CALLNUMBER').each(function () {              
-       var Loc = jQuery(this).parent().find('td.detailItemsTable_LOCATION').text();    
-       Loc = Loc.replace(/^\s+|\s+$/g, '');
-       console.log(Loc);
-       if(validSMLocs.indexOf(Loc) !== -1) {      
-          console.log("Matched!");
-          //jQuery( "<span id='shelfmap'><a href='https://app.shelfmap.co.uk/fp/fp?icode=44RCA&id=" + bibid + "' target='_blank' class='ShelfMap_anchor' title='Show item on ShelfMap'><img src='https://iainrca.github.io/summon/v2.svg' alt='ShelMap drop pin' height = '75px' width='75px'/>View Shelf Location</a></span></br>").insertAfter( jQuery(this) );
-          //jQuery(this).append( "<span id='shelfmap'><a href='https://app.shelfmap.co.uk/fp/fp?icode=44RCA&id=" + bibid + "' target='_blank' class='ShelfMap_anchor' title='Show item on ShelfMap' style='float:right'><img src='https://iainrca.github.io/summon/v2.svg' alt='ShelMap drop pin' height = '75px' width='75px' style='float:right'/>View Shelf Location</a></span></br>" );         
-          jQuery(this).append( "<span id='shelfmap'><a href='https://app.shelfmap.co.uk/fp/fp?icode=44RCA&id=" + bibid + "' target='_blank' class='ShelfMap_anchor' title='Show item on ShelfMap' style='float:right'>View Shelf Location</a></span></br>" );         
- 
-       }  
-     });
- 
-});
-
-  
